e02d88d13c754b9f5e20d10d765a65a6cb6f8ec3
Fredy <126272387+Fredidss@users.noreply.github.com>
2023-04-08 22:46:55 -0500

    Add files via upload

--- for2.py
+++ for2.py
@@ -1,30 +1,27 @@
-## calcular el promedio de edade de hombres, mujeres y todo un grupo de alumnos
-
 mujeres = 0
 hombres = 0
-contador = int(0)
-contador1 = int(0)
-contador2 = int(0)
+contador_mujeres = 0
+contador_hombres = 0
+contador_total = 0
 
 print("\n A TENER EN CUENTA: \n para mujer 1 \n para hombre 2")
 
 for x in range(1, 5):
-    edad = int(input("ingresa tu edad"))
-    genero = int(input("ingrese el numero correspondiente para su genero"))
+    edad = int(input("ingresa tu edad: "))
+    genero = int(input("ingrese el número correspondiente para su género (1 para mujer, 2 para hombre): "))
     if genero == 1:
-        contador1 += 1
+        mujeres += edad
+        contador_mujeres += 1
     elif genero == 2:
-        contador2 += 1
+        hombres += edad
+        contador_hombres += 1
 
-    contador1 == mujeres
-    contador2 == hombres
-    contador = contador + edad
-    promedio_mujeres = mujeres + edad * contador
-    promedio_hombres = hombres + edad * contador
+    contador_total += edad
 
-print(f"El promedio de edad del grupo es:", str(contador / 4));
-print(f"El promedio de edad de las mujeres es, {promedio_mujeres}")
-print(f"El promedio de edad de los hombres es, {promedio_hombres}")
+promedio_total = contador_total / 4
+promedio_mujeres = mujeres / contador_mujeres if contador_mujeres > 0 else 0
+promedio_hombres = hombres / contador_hombres if contador_hombres > 0 else 0
 
-
-    
+print(f"El promedio de edad del grupo es: {promedio_total}")
+print(f"El promedio de edad de las mujeres es: {promedio_mujeres}")
+print(f"El promedio de edad de los hombres es: {promedio_hombres}")
---
+++ for4.py
@@ -0,0 +1,6 @@
+
+
+
+
+MCD = match.gcd(34,15)
+print(f"el maximo comun divisor es, {MCD}")
